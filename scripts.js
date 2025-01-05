6e9129b3231d3eeeb77213f711b4ce1cac6617e4
citrusdeer956 <jasperncruz@protonmail.com>
2025-01-05 17:23:00 -0600

    Added hover function to change color of grids

--- scripts.js
+++ scripts.js
@@ -1,15 +1,25 @@
 const button = document.getElementById('button');
 const container = document.querySelector(".container");
 
-
 button.addEventListener('click', () => {
     // checks if square exceeds 5x5 limit
     if (container.children.length < 16) {
         const divSquare = document.createElement('div');
         divSquare.classList.add('grid');
         container.appendChild(divSquare);
-        console.log('this if statement is working!')
+        /*mouse hover event listener*/
+        divSquare.addEventListener('mouseover', function(event) {
+            this.style.backgroundColor = 'red';
+        });
+        divSquare.addEventListener('mouseout', function(event) {
+            this.style.backgroundColor = 'black';
+        });
     } else {
        alert("SQUARE FULL!");
-    }
+    }; 
 });
+
+
+
+
+
