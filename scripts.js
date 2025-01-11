73ceb9d1fef9200e797cf9aea66e2673e3894de4
citrusdeer956 <jasperncruz@protonmail.com>
2025-01-11 01:12:25 -0600

    Fixed bug where new grid button kept duplicating when grid was full

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 const button = document.getElementById('button');
 const container = document.querySelector(".container");
+const newGridBtn = button.cloneNode(true);
 
 button.addEventListener('click', () => {
     // checks if square exceeds 5x5 limit
     if (container.children.length < 16) {
+
         const divSquare = document.createElement('div');
         divSquare.classList.add('grid');
         container.appendChild(divSquare);
+
         /*mouse hover event listener*/
         divSquare.addEventListener('mouseover', function(event) {
             this.style.backgroundColor = 'red';
@@ -14,10 +17,27 @@ button.addEventListener('click', () => {
         divSquare.addEventListener('mouseout', function(event) {
             this.style.backgroundColor = 'black';
         });
-    } else {
-       alert("SQUARE FULL!");
-    }; 
+
+        
+    } else if (newGridBtn !== null){
+        /* new grid buttton */
+        newGridBtn.innerHTML = "press to create new grid"
+        newGridBtn.id = "GridBtn"
+        document.body.appendChild(newGridBtn);
+    };
 });
+
+/*new grid function */
+newGridBtn.addEventListener('click', () => {
+    newGridNum = parseInt(prompt("set a number for new grid:"));
+    if (newGridNum <= 100) {
+        alert('function works!')
+    } else {
+        alert('number is too big!');
+    }
+})
+
+
 
 
 
