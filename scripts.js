e68c5af0ab8cfa252691b367cbc99185a973bd1a
citrusdeer956 <jasperncruz@protonmail.com>
2025-01-25 13:30:21 -0600

    Added change color button to change between rainbow and black mode

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 const button = document.getElementById('button');
 const container = document.querySelector('.container');
 const btnClear = document.getElementById('btnClear');
+const btnColorToggle = document.getElementById('btnColor');
 const gridSize = 700;
 let mouseDrawing = false;
+let isRainbowColor = true; // tracks current brush mode
 
 // Function to generate a random color
 function getRandomColor() {
@@ -32,11 +34,11 @@ function createGrid(size) {
         // adds drawing interactivity
         divSquare.addEventListener('mousedown', () => {
             mouseDrawing = true;
-            divSquare.style.backgroundColor = getRandomColor();
+            divSquare.style.backgroundColor = isRainbowColor ? getRandomColor() : 'black';
         });
         divSquare.addEventListener('mouseover', () => {
             if (mouseDrawing) {
-                divSquare.style.backgroundColor = getRandomColor();
+                divSquare.style.backgroundColor = isRainbowColor ? getRandomColor() : 'black';
             }
         });
         
@@ -66,4 +68,14 @@ btnClear.addEventListener('click', () => {
     document.querySelectorAll('.grid').forEach(square => {
         square.style.backgroundColor = 'white';
     });
+});
+
+btnColorToggle.addEventListener('click', () => {
+    isRainbowColor = !isRainbowColor; // toggle the mode
+    if (isRainbowColor) {
+        alert('brush set to rainbow mode!');
+    } else {
+        alert('brush set to black mode!');
+    }
+    
 });
